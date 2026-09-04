2b8d1e42f8023cd5469dba746c3722d2ab6ddc12
syeda-hira-batool <ic.shirabatool09@gmail.com>
2026-09-04 11:54:41 +0500

    app.tsx and main updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { Routes, Route } from 'react-router-dom';
 import Background from './MyComponents/Background';
 import Categories from './MyComponents/Categories';
+import SortingHub from './MyComponents/Sortinghub';
+import SortVisualizer from './MyComponents/Sortvisualizer';
 
-export default function App() {
+function Home() {
   return (
     <>
       <Background />
       <Categories />
     </>
+  );
+}
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/sorting" element={<SortingHub />} />
+      <Route path="/sorting/:algoId" element={<SortVisualizer />} />
+    </Routes>
   );
 }
